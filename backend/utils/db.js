3c3f6b8870857b7dd9696f4d0de791f478fa02ba
MaheshSamale <maheshsamale7@gmail.com>
2026-01-22 11:13:12 +0530

    db.js database config

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,8 +3,10 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Rutuja0802@',
-    database: 'food_coupon_system'
-})
+    password: 'Mahesh@123',
+    database: 'food_coupon_system',
+    waitForConnections: true,
+    connectionLimit: 10
+});
 
-module.exports = pool
+module.exports = pool.promise(); // Important for async/await
